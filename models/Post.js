a837737db7f6ff7601475ae0d4e37357b6f898d7
Mike Willbanks <me@mike.engineer>
2019-01-01 14:52:11 -0600

    Remove arrow functions from models

--- models/Post.js
+++ models/Post.js
@@ -27,7 +27,9 @@ Post.add({
   categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
 });
 
-Post.schema.virtual('content.full').get(() => this.content.extended || this.content.brief);
+Post.schema.virtual('content.full').get(function() {
+  return this.content.extended || this.content.brief
+});
 
 Post.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
 Post.register();
